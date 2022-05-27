52f4143dbcb2898de7fb763594d79598fdf47fb3
fangchen <cfun.orz@gmail.com>
2022-05-26 22:18:38 -0700

    shell

--- script.sh
+++ script.sh
@@ -13,4 +13,4 @@
 
 # python ann.py --dataset CIFAR100 --batch_size 64 --architecture RESNET50 --learning_rate 1e-2 --epochs 300 --lr_interval '0.6 0.8 0.9' --lr_reduce 5 --optimizer SGD --dropout 0.2 --devices 0,1,2,3 --seed 0 --log
 
-python ann.py --dataset STL10 --batch_size 16 --architecture RESNET50 --im_size 224 --learning_rate 1e-2 --epochs 300 --lr_interval '0.6 0.8 0.9' --lr_reduce 5 --optimizer SGD --dropout 0.2 --devices 0 --seed 0 --log
+python ann.py --dataset STL10 --batch_size 8 --architecture RESNET50_NLP --im_size 224 --learning_rate 1e-2 --epochs 300 --lr_interval '0.6 0.8 0.9' --lr_reduce 5 --optimizer SGD --dropout 0.2 --devices 0,1 --seed 0 --log
