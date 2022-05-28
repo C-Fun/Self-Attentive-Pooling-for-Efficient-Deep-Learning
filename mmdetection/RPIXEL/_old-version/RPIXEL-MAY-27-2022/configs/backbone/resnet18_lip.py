7aa9f33fdd116b83ce4379137a252ab1ec393412
fangchen <cfun.orz@gmail.com>
2022-05-27 21:51:57 -0700

    mmdetection

--- mmdetection/RPIXEL/_old-version/RPIXEL-MAY-27-2022/configs/backbone/resnet18_lip.py
+++
@@ -1,9 +0,0 @@
-from RPIXEL.configs.backbone._base.resnet18 import _backbone, _convtype, conv1, pool1
-
-_pooltype = "lip"
-_poolparams = [
-				{'type':'none', 'stride':1},
-				{'type':_pooltype, 'ksize':2, 'stride':2},
-				{'type':_pooltype, 'ksize':2, 'stride':2},
-				{'type':_pooltype, 'ksize':2, 'stride':2}
-			]
--- mmdetection/RPIXEL/_old-version/RPIXEL-MAY-27-2022/configs/backbone/workshop_exp/resnet50_2222.py
+++
@@ -1,9 +0,0 @@
-from RPIXEL.configs.backbone._base.resnet50 import _backbone, _convtype, conv1, pool1
-
-_pooltype = "none"
-_poolparams = [
-				{'type':'none', 'stride':2},
-				{'type':'none', 'stride':2},
-				{'type':'none', 'stride':2},
-				{'type':'none', 'stride':2},
-			]
