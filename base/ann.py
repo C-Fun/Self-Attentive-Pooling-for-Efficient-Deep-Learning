c0dcba8ba9b75390b20086b25a14248afa75005a
fangchen <cfun.orz@gmail.com>
2023-06-12 15:13:18 -0700

    update

--- base/ann.py
+++ base/ann.py
@@ -19,9 +19,9 @@ import numpy as np
 import json
 import pickle
 
-# from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
-# from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-# from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
 
 from models.network import Network, name_parse
 
