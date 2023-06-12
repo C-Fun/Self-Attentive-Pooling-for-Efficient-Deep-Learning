e8ee09bb14e84c7d3f66398939d580cb2b13993f
fangchen <cfun.orz@gmail.com>
2023-06-12 14:46:00 -0700

    update

--- .backup/Non-Local-Pooling/mmdet_repo/configs/network/faster_rcnn/resnet50/resnet50.py
+++
@@ -1,4 +0,0 @@
-from mmdet_repo.configs.common.coco_detection import root as coco_root
-root = coco_root + '/Non-Local-Pooling/' # linux
-
-_base_ = [root + '/mmdet_repo/configs/network/faster_rcnn/_base_.py']
